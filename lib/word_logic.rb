317fb2857d2dd5098f251abf4461fb9e579e59d3
Tlotro <misha.pletneow@yandex.ru>
2023-05-18 20:23:24 +0300

    Upload Gem

---
+++ lib/word_logic.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "word_logic/version"
+
+module WordLogic
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/word_logic/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WordLogic
+  VERSION = "0.1.0"
+end
