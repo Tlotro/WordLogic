9da141f64492b03abb17022ad778ce054cfb3f27
Tlotro <50557602+Tlotro@users.noreply.github.com>
2023-05-24 13:44:24 +0000

    split code

--- lib/word_logic.rb
+++ lib/word_logic.rb
@@ -3,27 +3,35 @@
 require_relative "word_logic/version"
 
 module WordLogic
-  def getwords(length)
-    
-  end
+  def getwords(length); end
 
   def compareWords(chars, compchars)
+    compcharsres = compchars.clone
+    compchars.each { |e| compcharsres << e.dup }
     res = Array(chars.length)
-    for i in 1..chars.length
-      if (chars[i]==compchars[i])
-        compchars[i] = ' '
-        res[i] = 2
-      end
-    end
-    for i in 1..chars.length
-      if (compchars.include?(chars[i]))
-        compchars[i] = ' '
-        res[i] = 1
-      end
-    end
+    checkGreen(chars, compcharsres, res)
+    checkYellow(chars, compcharsres, res)
     res
   end
-  
+
+  def checkGreen(chars, compchars, res)
+    for i in 1..chars.length
+      if chars[i] == compchars[i]
+        compchars[i] = " "
+        res[i] = 2
+      end
+      end
+  end
+
+  def checkYellow(chars, compchars, res)
+    for i in 1..chars.length
+      if ompcharsres.include?(chars[i])
+        compchars[i] = " "
+        res[i] = 2
+      end
+      end
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
